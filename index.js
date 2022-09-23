08902b8db1d629994a2c62bf6637243687f282e8
MD SAHADUZZAMAN <sahaduzzaman.cse@gmail.com>
2022-09-23 12:11:47 +0600

    make it better

--- index.js
+++ index.js
@@ -14,16 +14,21 @@ app.use(cors());
 
 app.use("/posts", postRoutes);
 
-app.get('/', (req, res) =>{
-  res.send('Hello to Memories Api');
+app.get("/", (req, res) => {
+  res.send("Hello to Memories Api");
 });
-
 
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() =>
     app.listen(PORT, () => console.log(`Server Running on Port: ${PORT}`))
   )
   .catch((error) => console.log(error));
+
+
+mongoose.set("useFindAndModify", false);
